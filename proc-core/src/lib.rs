bb77583d2f5f88381f482b650bf13076e4d7f8fe
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2026-02-23 18:09:08 +0200

    [HEAVY WIP] Added a proc-macro extension, still working on it

---
+++ proc-core/src/lib.rs
@@ -0,0 +1,19 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro]
+pub fn cron(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = &input.ident;
+
+    let expanded = quote! {
+        impl #name {
+            pub fn greet() -> String {
+                my_library::hello(stringify!(#name))
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
