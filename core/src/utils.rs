28c72c48d0937b512d9b514f4f9f2e2e8bc556c5
Abhishek shivale <abhishekshivale45@gmail.com>
2026-02-26 21:12:15 +0530

    docs: for task indentifier trait

--- core/src/utils.rs
+++ core/src/utils.rs
@@ -37,6 +37,53 @@ macro_rules! define_event {
     };
 }
 
+/// TaskIdentifier trait use for defining unique indentifier for example [Uuid], integers, strings, and generally any kind of identifier format the user can use which suits their needs.
+///
+/// # Semantics
+/// Implementors must provide a way to generate unique indentifier for task.
+///
+/// # Required Subtrait(s)
+/// TaskIdentifier requires the following subtraits in order to be implemented:
+/// - `Debug` - For displaying the ID.
+/// - `Clone` - For fully cloning the ID.
+/// - `PartialEq` - For comparing 2 IDs and checking if they are equal.
+/// - `Eq` - For ensuring the comparison applies in both directions.
+/// - `Hash` - For producing a hash from the ID.
+///
+/// TaskIdentifier also requires `Send` + `Sync` + `'static`.
+///
+/// # Required Method(s)
+/// - [`generate`](TaskIdentifier::generate) - Produces a new unique identifier.
+///
+/// # Implementation(s)
+/// - [`Uuid`] - Generates a random UUID v4 via [`Uuid::new_v4`].
+///
+/// # Object Safety / Dynamic Dispatching
+/// This trait is **not** object-safe due to the `Clone` and `Sized` requirement from its supertraits.
+///
+/// # Example(s)
+///
+/// ```
+/// use uuid::Uuid;
+/// use chronographer::utils::TaskIdentifier;
+/// 
+/// #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+/// struct TaskId(Uuid);
+/// impl TaskIdentifier for TaskId {
+///     fn generate() -> Self {
+///         TaskId(Uuid::new_v4())
+///     }
+/// }
+/// 
+/// let task_id = TaskId::generate();
+/// let id: Uuid = Uuid::generate();
+/// 
+/// assert_ne!(id, task_id.0);
+/// ```
+/// # See Also
+/// - [`Uuid`] - The default implementation, generating random v4 UUIDs.
+/// - [`Task`](crate::task::Task) - The primary consumer of task identifiers.
+
 pub trait TaskIdentifier:
     'static + Debug + Clone + Eq + PartialEq<Self> + Hash + Send + Sync
 {
